6a62b9c527108ad7d6c643481b65e645a8e42955
Tomaszsachanowski <tomaszsachanowski@gmail.com>
2022-05-26 01:59:56 +0200

    Save current work

--- main_in_works.py
+++ main_in_works.py
@@ -1,6 +1,7 @@
 import click
 
-# from ner import training as ner_training
+from ner import training as ner_training
+from config_parser import get_config
 # import pandas as pd
 
 # from relations_model import train_model, evaluate_model
@@ -17,13 +18,19 @@ def bert_cli():
 
 @bert_cli.command(
 help="""Train M-BERT model for named entity recognition (NER) and
-relations extraction (RE) with train file test file and config file
-""", short_help="Train M-BERT model")
-@click.option('--config', default='./config/train.cfg', type=str, help='path to train config')
-@click.option('--model_name', default=1, type=str, help='model name')
+relations extraction (RE) with train file test file and config file""",
+short_help="Train M-BERT model")
+@click.option('--config', default='./config/train.cfg',
+              type=str, help='path to train config')
+@click.option('--model_name', default="en-small-corpora",
+              type=str, help='model name')
 @click.argument('train_file', nargs=1, required=True, type=str)
 @click.argument('test_file', nargs=1, required=True, type=str)
 def train(train_file, test_file, config, model_name):
+    config_file = get_config(config)
+    ner_training.train_model(
+        train_tsv_file=train_file, test_tsv_file=test_file,
+        model_name=model_name, training_arguments=config_file['train']['ner'])
     print(train_file)
     print(test_file)
     print(config)
